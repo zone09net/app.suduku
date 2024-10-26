32071183657651c652660d2b2cdd815e5234a603
zone09net <rcote@zone09.net>
2024-10-25 22:44:22 -0400

    modified config files

--- rollup.config.js
+++ rollup.config.js
@@ -8,7 +8,10 @@ export default [
 				'@zone09.net/foundation': './lib.foundation.js',
 				'@zone09.net/paperless': './lib.paperless.js',
 				'@zone09.net/hac': './lib.hac.js',
-				'@extlib/sudukogen': './extlib/./extlib/sudukugen-1.0.2.js',
+				'@extlib/matter': './extlib/matter-0.20.0.min.js',
+				'@extlib/poly-decomp': './extlib/poly-decomp-0.2.1.min.js',
+				'@extlib/intersects': './extlib/intersects-2.7.1.min.js',
+				'@extlib/sudukogen': './extlib/sudukugen-1.0.2.min.js'
 			 }
 		},
 	},
